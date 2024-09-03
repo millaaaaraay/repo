6bf3d183378afc05f871a9541ad164946c29691f
matias12rt <matia.orellanac@duocuc.cl>
2024-09-03 18:07:28 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TiendaApp',
+  webDir: 'www'
+};
+
+export default config;
